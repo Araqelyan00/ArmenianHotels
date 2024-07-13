db7462fbcbfb15674701570e477612c03cd76f8a
Araqelyan00 <aramaraqelyan503@gmail.com>
2024-07-13 19:24:33 +0400

    new

--- src/exception/HotelNotFoundException.java
+++ src/exception/HotelNotFoundException.java
@@ -1,4 +1,17 @@
 package exception;
 
-public class HotelNotFoundException {
+public class HotelNotFoundException extends Exception{
+    public HotelNotFoundException() {}
+    public HotelNotFoundException(String message) {
+        super(message);
+    }
+    public HotelNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public HotelNotFoundException(Throwable cause) {
+        super(cause);
+    }
+    public HotelNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/storage/RoomStorage.java
+++ src/storage/RoomStorage.java
@@ -6,13 +6,45 @@ import model.RoomTypes;
 
 public class RoomStorage {
 
-    private static Room[] rooms = new Room[10];
+    private static final Room[] rooms = new Room[10];
 
 
-    public void printRoomTypes(ArmenianHotel hotel){
+    public void printRoomTypes(){
         for (RoomTypes type : RoomTypes.values()) {
             System.out.println(type.name());
         }
     }
 
-}
+    public void printAvailableRoomsByType(ArmenianHotel hotel, RoomTypes type) {
+        for (Room room : rooms) {
+            if (hotel.getRooms().getType() == type && !hotel.getRooms().isReserved()) {
+                System.out.println(room.getNumber());
+            }
+        }
+    }
+
+    public void reserveRoom(Room room) {
+        if (!room.isReserved()) {
+            room.setReserved(true);
+            System.out.println("Room " + room.getNumber() + " reserved successfully");
+        }else {
+            room.setReserved(false);
+            System.out.println("You can't reserve room " + room.getNumber());
+        }
+    }
+
+    public void releaseRoom(Room room) {
+        if (room.isReserved()) {
+            room.setReserved(false);
+            System.out.println("Room " + room.getNumber() + " released successfully");
+        }else {
+            room.setReserved(true);
+            System.out.println("Room number " + room.getNumber() + " released");
+        }
+    }
+
+
+
+    }
+
+
